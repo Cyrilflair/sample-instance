fd6de47871a4f5fbe4905f970091a1c8a60088b0
Cyril flair <78265858+Cyrilflair@users.noreply.github.com>
2022-01-04 10:23:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 resource "aws_instance" "web_server_ec2" {
   ami = "ami-0851b76e8b1bce90b"
-	instance_type = "t2.micro"
+  instance_type = "t2.micro"
   availability_zone = "ap-south-1a"
-	key_name = "keypair"
+  key_name = "keypair"
   subnet_id = "subnet-0ae55cba67cf72ec2"
   vpc_security_group_ids = ["sg-0fa19f815cca59b5b"]
   associate_public_ip_adress = true
