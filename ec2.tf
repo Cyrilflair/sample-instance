97cbd2bf2e861bf706f6ef8c5fd023a5ed73f8f9
Cyril flair <78265858+Cyrilflair@users.noreply.github.com>
2022-01-04 10:32:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "web_server_ec2" {
-  ami = "ami-0851b76e8b1bce90b"
-  instance_type = "t2.micro"
-  availability_zone = "ap-south-1a"
-  key_name = "keypair"
-  subnet_id = "subnet-0ae55cba67cf72ec2"
-  vpc_security_group_ids = ["sg-0fa19f815cca59b5b"]
-  associate_public_ip_adress = true
-  
+resource "aws_instance" "public-web-ec2" {
+  ami                         = "ami-052cef05d01020f1d"
+  instance_type               = "t2.micro"
+  availability_zone           = "ap-south-1a"
+  key_name                    = "keypair"
+  subnet_id                   = "subnet-0ae55cba67cf72ec2"
+  vpc_security_group_ids      = ["sg-0fa19f815cca59b5b"]
+  associate_public_ip_address = true
+
 }
